6c1bb96f364481f307723cd2409eb7a452136d3e
Andreas Pirkner <andreas.pirkner@gmx.net>
2020-06-26 16:52:51 +0200

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { TravelsComponent } from './travels/travels.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    TravelsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+ {
+   picture: '../assets/img/travel1.jpg',
+   destination: 'Grand Canyon Tour',
+   description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ',
+   dates: '23.08.2020,02.09.2020,',
+   duration: '14 Days',
+   price: '1.699'
+ },
+ {
+   picture: '/assets/img/travel2.jpg',
+   destination: 'San Francisco Delight',
+   description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ',
+   dates: '16.08.2020,25.09.2020,',
+   duration: '7 Days',
+   price: '1.199'
+ },
+ {
+   picture: 'assets/img/travel3.jpg',
+   destination: 'Greece Island Hopping',
+   description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. ',
+   dates: '27.08.2020,07.09.2020,',
+   duration: '21 Days',
+   price: '1.999'
+ }
+];
